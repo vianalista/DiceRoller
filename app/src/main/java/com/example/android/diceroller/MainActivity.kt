cc9779d20fead0069872ef5c4e763925c9e64b42
Wandick Vinícius Antunes Lopes <vianalista@gmail.com>
2019-06-11 16:10:02 -0300

    OnClickListener.

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.android.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var myRollButton: Button = findViewById(R.id.roll_button)
+        //myRollButton.text = "Let's roll"
+        myRollButton.setOnClickListener {
+            Toast.makeText(this, "teste", Toast.LENGTH_SHORT).show()
+        }
     }
 }
