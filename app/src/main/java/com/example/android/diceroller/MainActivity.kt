0a3b98fdb3fc1e78559608cd781282fa40cf00e6
Wandick Vinícius Antunes Lopes <vianalista@gmail.com>
2019-06-11 17:05:25 -0300

    Create function, use Random function.

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.android.diceroller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var myRollButton: Button = findViewById(R.id.roll_button)
+
         //myRollButton.text = "Let's roll"
         myRollButton.setOnClickListener {
-            Toast.makeText(this, "teste", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, "Does something when the button is clicked.", Toast.LENGTH_SHORT).show()
+            myFunc()
         }
     }
+
+
+    private fun myFunc() {
+        val resultText: TextView = findViewById(R.id.idTView1)
+        //Use the [Random] from (import java.util.*), no the [Random] from Kotlin.
+        val randomInt = Random().nextInt(6)
+        resultText.text = randomInt.toString()
+    }
+
 }
